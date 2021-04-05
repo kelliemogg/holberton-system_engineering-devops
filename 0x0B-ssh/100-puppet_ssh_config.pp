724ce1fa070bf30483edad4dc9283d35eda41865
Kellie Mogg <kelliedmogg@gmail.com>
2021-04-05 01:29:51 +0000

    changed file to path

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,5 @@
 # Puppet file to make changes to a config file 
-file { 'ssh_config':
-  path    => /etc/ssh/ssh_config
+file { '/etc/ssh/ssh_config':
   content => 'PasswordAuthentication no
   IdentityFile ~/.ssh/holberton',
 }
