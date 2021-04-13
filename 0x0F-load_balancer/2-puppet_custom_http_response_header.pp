74958d180cd63e8ad823aacb913fe65caf354bc1
Kellie Mogg <kelliedmogg@gmail.com>
2021-04-13 18:50:05 +0000

    syntax

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,27 +1,27 @@
 # create a custum HTTP header response
 exec { 'update':
-    command => 'apt-get update',
-    user    => 'root',
+    command  => 'apt-get update',
+    user     => 'root',
     provider => 'shell'
 }
 ->
 
 package { 'nginx':
-    ensure => present,
-    name => 'nginx',
+    ensure   => present,
+    name     => 'nginx',
     provider => 'apt'
 }
 ->
 
 exec { 'Add header':
-    command => 'sed -i "48i add_header X-Served-By \$HOSTNAME;" /etc/nginx/sides-available/default',
-    user => 'root',
+    command  => 'sed -i "48i add_header X-Served-By \$HOSTNAME;" /etc/nginx/sides-available/default',
+    user     => 'root',
     provider => 'shell'
 }
 ->
 
 exec { 'Start nginx':
-    command => 'service nginx restart,
-    user => 'root',
+    command  => 'service nginx restart,
+    user     => 'root',
     provider => 'shell'
 }
