bc6096dc71f08875fa232620e491ff943916b9ff
Kellie Mogg <kelliedmogg@gmail.com>
2021-05-18 22:18:11 +0000

    added allow_redirects = False

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ def number_of_subscribers(subreddit):
     """ returns number of subscribers """
     subURL = 'https://www.reddit.com/r/{}/about/.json'.format(subreddit)
     headers = {"User-Agent": "user"}
-    getDict = requests.get(subURL, headers=headers).json()
+    getDict = requests.get(subURL, headers=headers, allow_redirects=False).json()
 
     if "data" not in getDict:
         return 0
