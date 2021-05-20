f5809d1955e01ca83459c65ed5fae421eb6c7703
Kellie Mogg <kelliedmogg@gmail.com>
2021-05-20 02:11:40 +0000

    correct output task 2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -5,19 +5,30 @@ import json
 import requests
 
 
-def recurse(subreddit, hot_list=[]):
+def recurse(subreddit, hot_list=[], count=0, after=None):
     """ uses recursive function to query Reddit API """
+    # passes in subreddit as arg to our url
     hotURL = 'https://www.reddit.com/r/{}/hot/.json'.format(subreddit)
+    # reddit is picky about headers
     headers = {"User-Agent": "user"}
-    getDict = requests.get(
+    # gets correct json data for our desired subreddit page
+    # allow_redirects is a boolean to disable redirection
+    # Request: From Client to Server & Response: From Server to Client
+    response = requests.get(
         hotURL,
         headers=headers,
         allow_redirects=False).json()
-    forData = getDict.get("data")
 
-    if 'data' not in getDict:
-        print("None")
+    forData = response.get("data")
+
+    if 'data' not in response:
+        return (None)
     else:
-        forChildren = forData.get("children")
-        for titles in forChildren[:10]:
-            print(titles.get('data')['title']) 
+        children = forData.get("children")
+        for child in children:
+            this = child.get("data").get("title")
+            hot_list.append(this)
+            count += 1
+    if after is None:
+        return hot_list    
+    return (recurse(subreddit, hot_list, count, after))
